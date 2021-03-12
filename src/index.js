0ab9c1261bb6df00373ec5ab1e4f4e22b835767f
Андрей Жданов <cuter.exe@gmail.com>
2021-03-12 17:54:21 +0300

    refactor: implement towelSort function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-
 // You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!matrix) {
+        return [];
+    }
+    return matrix.reduce(
+        (acc, currentValue, index) =>
+            index % 2 == 0
+                ? acc.concat(currentValue)
+                : acc.concat(currentValue.reverse()),
+        []
+    );
+};
